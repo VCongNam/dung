1520e3d64d5e21c21c4538ec15725010e8f9af1f
VCNam <namvucong23@gmail.com>
2024-08-21 18:08:50 +0700

    22th commit

--- src/Components/MainDish/Dish.jsx
+++ src/Components/MainDish/Dish.jsx
@@ -6,7 +6,7 @@ const Dish = () => {
         <div className='backCard'>
             <div class="card-body">
                 <h5 class="money">123.000 VND/ Người</h5>
-                <p className='sub-text'>Lựa chọn loại giẩm bạn thích: </p>
+                <p className='sub-text'>Lựa chọn loại giấm bạn thích: </p>
                 <ul className='sub-text'>
                     <li>Giấm vải thiều <span className="dot"></span></li>
                     <li>Giấm táo mèo</li>
